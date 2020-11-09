c28483c74fdc98c980d39e53f653baa954ce2119
iaroki <iaroki@protonmail.com>
2020-11-09 19:42:17 +0200

    update config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -26,9 +26,7 @@
   environment.systemPackages = with pkgs; [
     wget vim tmux git firefox remmina mpv htop xarchiver vagrant tdesktop tree rofi
     xfce.xfce4-battery-plugin xfce.xfce4-clipman-plugin xfce.xfce4-datetime-plugin
-    xfce.xfce4-embed-plugin xfce.xfce4-namebar-plugin xfce.xfce4-netload-plugin
-    xfce.xfce4-sensors-plugin xfce.xfce4-systemload-plugin xfce.xfce4-verve-plugin
-    xfce.xfce4-weather-plugin xfce.xfce4-xkb-plugin xfce.xfdashboard xorg.xhost
+    xfce.xfce4-xkb-plugin xfce.xfdashboard xorg.xhost unzip polybar bspwm
   ];
 
   environment.homeBinInPath = true;
@@ -52,6 +50,32 @@
   services.xserver.displayManager.defaultSession = "xfce";
   services.xserver.desktopManager.xfce.thunarPlugins = [ pkgs.xfce.thunar-archive-plugin pkgs.xfce.thunar-volman ];
 
+  services.tlp = {
+    enable = true;
+    settings = {
+
+      CPU_SCALING_GOVERNOR_ON_AC="performance";
+      CPU_SCALING_GOVERNOR_ON_BAT="powersave";
+
+        # Enable audio power saving for Intel HDA, AC97 devices (timeout in secs).
+        # A value of 0 disables, >=1 enables power saving (recommended: 1).
+        # Default: 0 (AC), 1 (BAT)
+      SOUND_POWER_SAVE_ON_AC=0;
+      SOUND_POWER_SAVE_ON_BAT=1;
+
+        # Runtime Power Management for PCI(e) bus devices: on=disable, auto=enable.
+        # Default: on (AC), auto (BAT)
+      RUNTIME_PM_ON_AC="on";
+      RUNTIME_PM_ON_BAT="auto";
+
+        # Battery feature drivers: 0=disable, 1=enable
+        # Default: 1 (all)
+      NATACPI_ENABLE=1;
+      TPACPI_ENABLE=1;
+      TPSMAPI_ENABLE=1;
+    };
+  };
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.msytnyk = {
     isNormalUser = true;
@@ -65,7 +89,7 @@
   virtualisation.virtualbox.host.enable = true;
 #  virtualisation.virtualbox.host.enableExtensionPack = true;
 
-  system.stateVersion = "20.03"; # Did you read the comment?
+  system.stateVersion = "20.09";
   nixpkgs.config.allowUnfree = true;
 
 }
