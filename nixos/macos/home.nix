6313fe75e1551cbee118e5509212314a20b91ee5
Maxim Sytnyk <iaroki@protonmail.com>
2021-11-19 14:42:39 +0200

    remove neovim extra config

--- nixos/macos/home.nix
+++ nixos/macos/home.nix
@@ -62,39 +62,6 @@ home.packages = [
             viAlias = true;
             vimAlias = true;
             vimdiffAlias = true;
-            extraConfig = ''
-                runtime ./plug.vim
-                map <C-n> :NvimTreeToggle<CR>
-                inoremap kj <esc>
-                cnoremap kj <C-C>
-                colorscheme gruvbox
-                set background=dark
-                set termguicolors
-                set number
-                syntax on
-                set encoding=UTF-8
-                set laststatus=2
-                set noshowmode
-                set incsearch
-                set hlsearch
-                set smarttab
-                set expandtab
-                set tabstop=2
-                set shiftwidth=2
-                set softtabstop=2
-                set smartindent
-                set ignorecase
-                set smartcase
-                set scrolloff=8
-                set cursorline
-                set splitbelow
-                set splitright
-                let g:indentLine_color_dark = 1
-                let g:indentLine_char = '┊'
-                let g:better_whitespace_enabled=1
-                let g:strip_whitespace_on_save=1
-                let g:rainbow_active = 1
-            '';
         };
 
         programs.bash = {
