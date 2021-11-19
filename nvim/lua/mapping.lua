025bae5192e11ed16e631243068c2c6620b604e3
Maxim Sytnyk <iaroki@protonmail.com>
2021-11-19 15:23:31 +0200

    add terminal keymap

--- nvim/lua/mapping.lua
+++ nvim/lua/mapping.lua
@@ -1,6 +1,7 @@
 vim.api.nvim_set_keymap('', '<C-n>', ':NvimTreeToggle<CR>', {noremap = false})
 vim.api.nvim_set_keymap('i', 'kj', '<esc>', {noremap = true})
 vim.api.nvim_set_keymap('c', 'kj', '<C-C>', {noremap = true})
+vim.api.nvim_set_keymap('t', '<esc>', '<C-\\><C-n>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', {noremap = true})
