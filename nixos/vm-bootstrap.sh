70270c6d9d2b2e619c7c5f6055b2fc31d6e472c8
iaroki <iaroki@protonmail.com>
2022-01-26 14:13:15 +0200

    fix typos

--- nixos/vm-bootstrap.sh
+++ nixos/vm-bootstrap.sh
@@ -3,8 +3,8 @@ sudo parted /dev/sda -- mklabel gpt
 sudo parted /dev/sda -- mkpart primary 512MiB 100%
 sudo parted /dev/sda -- mkpart esp fat32 1MiB 512MiB
 sudo parted /dev/sda -- set 2 esp on
-sudo mkfs.ext4 -l nixos /dev/sda1
-sudo mkfs.fat -f 32 -n boot /dev/sda2
+sudo mkfs.ext4 -L nixos /dev/sda1
+sudo mkfs.fat -F 32 -n boot /dev/sda2
 sudo mount /dev/disk/by-label/nixos /mnt
 sudo mkdir -p /mnt/boot
 sudo mount /dev/disk/by-label/boot /mnt/boot
