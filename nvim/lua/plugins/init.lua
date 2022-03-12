d629f5b9c37e823d86bc68f32a9b8b09f138a231
iaroki <iaroki@protonmail.com>
2022-03-12 11:58:22 +0200

    refactor neovim plugins

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -5,67 +5,32 @@ if fn.empty(fn.glob(install_path)) > 0 then
 end
 
 return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use {
-    'catppuccin/nvim',
-    as = 'catppuccin'
-  }
-  use 'gruvbox-community/gruvbox'
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate'
-  }
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = {
-      'kyazdani42/nvim-web-devicons', 
-      opt = true
-    }
-  }
-  use {
-    'akinsho/bufferline.nvim',
-    requires = 'kyazdani42/nvim-web-devicons'
-  }
-  use {
-    'kyazdani42/nvim-tree.lua',
-    requires = {
-      'kyazdani42/nvim-web-devicons',
-    }
-  }
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = {
-      'nvim-lua/plenary.nvim'
-    }
-  }
-  use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-vsnip'
-  use 'hrsh7th/vim-vsnip'
-  use 'onsails/lspkind-nvim'
-  use 'p00f/nvim-ts-rainbow'
-  use 'phaazon/hop.nvim'
-  use 'numToStr/Comment.nvim'
-  use {
-    'lewis6991/gitsigns.nvim',
-    requires = {
-      'nvim-lua/plenary.nvim'
-    },
-  }
-  use 'windwp/nvim-autopairs'
-  use 'lukas-reineke/indent-blankline.nvim'
-  use {
-    'folke/todo-comments.nvim',
-    requires = 'nvim-lua/plenary.nvim'
-  }
-  use 'norcalli/nvim-colorizer.lua'
+  use {'wbthomason/packer.nvim'}
+  use {'catppuccin/nvim', as = 'catppuccin'}
+  use {'gruvbox-community/gruvbox'}
+  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+  use {'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true}}
+  use {'akinsho/bufferline.nvim', requires = {'kyazdani42/nvim-web-devicons'}}
+  use {'kyazdani42/nvim-tree.lua', requires = {'kyazdani42/nvim-web-devicons'}}
+  use {'nvim-telescope/telescope.nvim', requires = {'nvim-lua/plenary.nvim'}}
+  use {'neovim/nvim-lspconfig'}
+  use {'hrsh7th/cmp-nvim-lsp'}
+  use {'hrsh7th/cmp-buffer'}
+  use {'hrsh7th/cmp-path'}
+  use {'hrsh7th/cmp-cmdline'}
+  use {'hrsh7th/nvim-cmp'}
+  use {'hrsh7th/cmp-vsnip'}
+  use {'hrsh7th/vim-vsnip'}
+  use {'onsails/lspkind-nvim'}
+  use {'p00f/nvim-ts-rainbow'}
+  use {'phaazon/hop.nvim'}
+  use {'numToStr/Comment.nvim'}
+  use {'lewis6991/gitsigns.nvim', requires = {'nvim-lua/plenary.nvim'}}
+  use {'windwp/nvim-autopairs'}
+  use {'lukas-reineke/indent-blankline.nvim'} 
+  use {'folke/todo-comments.nvim', requires = {'nvim-lua/plenary.nvim'}}
+  use {'norcalli/nvim-colorizer.lua'}
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
   if packer_bootstrap then
     require('packer').sync()
   end
