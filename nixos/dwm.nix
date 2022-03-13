cc18ab20762c5ea445f7b88d42e9b453a7b20f32
Maxim Sytnyk <iaroki@protonmail.com>
2022-03-13 13:05:22 +0200

    replace dwm-status with slstatus

--- nixos/dwm.nix
+++ nixos/dwm.nix
@@ -2,31 +2,40 @@
 
 {
 
-nixpkgs.overlays = [
-  (self: super: {
-    dwm = super.dwm.overrideAttrs (oldAttrs: rec {
-      src = super.fetchFromGitHub {
-        owner = "iaroki";
-        repo = "dwm";
-        rev = "d9aaf84d190d77a391ae4cee49d24815d5cbb4b3";
-        sha256 = "0qvjqragad0bz5gj2qaxpb1wig88wmr90hkc70prz0s6y0g99sil";
-      };
-    });
+  nixpkgs.overlays = [
+    (self: super: {
+      dwm = super.dwm.overrideAttrs (oldAttrs: rec {
+        src = super.fetchFromGitHub {
+          owner = "iaroki";
+          repo = "dwm";
+          rev = "d9aaf84d190d77a391ae4cee49d24815d5cbb4b3";
+          sha256 = "0qvjqragad0bz5gj2qaxpb1wig88wmr90hkc70prz0s6y0g99sil";
+        };
+      });
 
-    st = super.st.overrideAttrs (oldAttrs: rec {
-      src = super.fetchFromGitHub {
-        owner = "iaroki";
-        repo = "st";
-        rev = "dfdef1ae30fb02c9f70b8f241f812feba6ca6224";
-        sha256 = "16a13bn7i6xr5sbzchimjc6r6fn491y3g38nqg34mr21snwnrw95";
-      };
-      buildInputs = oldAttrs.buildInputs ++  [ super.harfbuzz ];
-    });
+      st = super.st.overrideAttrs (oldAttrs: rec {
+        src = super.fetchFromGitHub {
+          owner = "iaroki";
+          repo = "st";
+          rev = "dfdef1ae30fb02c9f70b8f241f812feba6ca6224";
+          sha256 = "16a13bn7i6xr5sbzchimjc6r6fn491y3g38nqg34mr21snwnrw95";
+        };
+        buildInputs = oldAttrs.buildInputs ++  [ super.harfbuzz ];
+      });
+
+      slstatus = super.slstatus.overrideAttrs (oldAttrs: rec {
+        src = super.fetchFromGitHub {
+          owner = "iaroki";
+          repo = "slstatus";
+          rev = "de91b3f738fcd42f88ad4653c3ac20ab1564961d";
+          sha256 = "00lpdw1d4r1sn4ccw4dr5qw7m7fcv19ma1ajw5jbm6vcpnm9x74f";
+        };
+      });
     })
   ];
 
   environment.systemPackages = with pkgs; [
-    xorg.xhost dmenu st dwm dwm-status xclip rofi
+    xorg.xhost dmenu st dwm slstatus xclip rofi
   ];
 
   fonts.fonts = [
