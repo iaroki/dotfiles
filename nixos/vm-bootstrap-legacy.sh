ea285a19a17a036c3fd59f5d92f1adaed6dbefd9
Maxim Sytnyk <iaroki@protonmail.com>
2022-03-15 17:16:23 +0200

    add nixos mbr bootstrap

--- nixos/vm-bootstrap-legacy.sh
+++
@@ -1,6 +0,0 @@
-
-sudo parted /dev/sda -- mklabel msdos
-sudo parted /dev/sda -- mkpart primary 1MiB 100%
-sudo mkfs.ext4 -L nixos /dev/sda1
-sudo mount /dev/disk/by-label/nixos /mnt
-sudo nixos-generate-config --root /mnt
