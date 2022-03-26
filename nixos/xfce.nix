ee724fc9e2cdef66fbc9dde90578aab9a76d1d9d
iaroki <iaroki@protonmail.com>
2022-03-26 17:29:20 +0200

    add pipewire and bluetooth

--- nixos/xfce.nix
+++ nixos/xfce.nix
@@ -2,9 +2,10 @@
 
 {
   environment.systemPackages = with pkgs; [
-  firefox remmina zathura mpv xorg.xhost
+  firefox remmina zathura mpv xorg.xhost rofi tdesktop pulseaudio pavucontrol pasystray
   xfce.xfce4-battery-plugin xfce.xfce4-clipman-plugin xfce.xfce4-datetime-plugin
-  xfce.xfce4-xkb-plugin xfce.xfdashboard
+  xfce.xfce4-xkb-plugin xfce.xfce4-volumed-pulse xfce.xfce4-pulseaudio-plugin
+  xfce.xfdashboard
   ];
 
   fonts.fonts = [
@@ -17,8 +18,41 @@
   programs.slock.enable = true;
   programs.nm-applet.enable = true;
 
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
+  sound.enable = false;
+
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    media-session.enable = true;
+    media-session.config.bluez-monitor.rules = [
+      {
+        matches = [ { "device.name" = "~bluez_card.*"; } ];
+        actions = {
+          "update-props" = {
+            "bluez5.reconnect-profiles" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
+            "bluez5.msbc-support" = true;
+            "bluez5.sbc-xq-support" = true;
+          };
+        };
+      }
+      {
+        matches = [
+          { "node.name" = "~bluez_input.*"; }
+          { "node.name" = "~bluez_output.*"; }
+        ];
+        actions = {
+          "node.pause-on-idle" = false;
+        };
+      }
+    ];
+  };
+
+  hardware.enableAllFirmware = true;
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
 
   services.xserver.enable = true;
   services.xserver.layout = "us,ru";
