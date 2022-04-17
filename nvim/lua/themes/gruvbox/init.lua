f4e4a8bd883e9ef489214b5f773b63ef8478d1e6
iaroki <iaroki@protonmail.com>
2022-04-17 12:27:32 +0300

    move to gruvbox-material

---
+++ nvim/lua/themes/gruvbox/init.lua
@@ -0,0 +1,7 @@
+
+vim.o.background = 'dark'
+vim.g.gruvbox_material_background = 'hard'
+vim.g.gruvbox_material_palette = 'original'
+vim.g.gruvbox_material_statusline_style = 'original'
+vim.g.gruvbox_material_better_performance = 1
+vim.cmd('colorscheme gruvbox-material')
