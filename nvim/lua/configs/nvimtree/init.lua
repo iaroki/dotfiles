017c8b5682b8f166b5dd637030cca7e7f8d148c6
Maxim Sytnyk <iaroki@protonmail.com>
2022-05-03 11:58:33 +0300

    remove deprecated options

--- nvim/lua/configs/nvimtree/init.lua
+++ nvim/lua/configs/nvimtree/init.lua
@@ -7,10 +7,6 @@ require'nvim-tree'.setup {
   open_on_tab          = false,
   hijack_cursor        = false,
   update_cwd           = false,
-  update_to_buf_dir    = {
-    enable = true,
-    auto_open = true,
-  },
   diagnostics = {
     enable = false,
     icons = {
@@ -43,7 +39,6 @@ require'nvim-tree'.setup {
     height = 30,
     hide_root_folder = false,
     side = 'left',
-    auto_resize = false,
     mappings = {
       custom_only = false,
       list = {}
