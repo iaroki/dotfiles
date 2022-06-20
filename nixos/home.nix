86f1919fd144676b2c7b2e7c36c50252379c81e7
Maxim Sytnyk <iaroki@protonmail.com>
2022-06-20 22:50:24 +0300

    tmux kanagawa

--- nixos/home.nix
+++ nixos/home.nix
@@ -141,29 +141,24 @@ in
       terminal = "xterm-256color";
       secureSocket = false;
       extraConfig = ''
-        set-option -g status "on"
-        set-option -g status-style bg=colour237,fg=colour223
-        set-window-option -g window-status-style bg=colour214,fg=colour237
-        set-window-option -g window-status-activity-style bg=colour237,fg=colour248
-        set-window-option -g window-status-current-style bg=red,fg=colour237
-        set-option -g pane-active-border-style fg=colour250
-        set-option -g pane-border-style fg=colour237
-        set-option -g message-style bg=colour239,fg=colour223
-        set-option -g message-command-style bg=colour239,fg=colour223
-        set-option -g display-panes-active-colour colour250
-        set-option -g display-panes-colour colour237
-        set-window-option -g clock-mode-colour colour109
-        set-window-option -g window-status-bell-style bg=colour167,fg=colour235
-        set-option -g status-justify "left"
-        set-option -g status-left-style none
-        set-option -g status-left-length "80"
-        set-option -g status-right-style none
-        set-option -g status-right-length "80"
-        set-window-option -g window-status-separator ""
-        set-option -g status-left "#[bg=colour241,fg=colour248] #S #[bg=colour237,fg=colour241,nobold,noitalics,nounderscore]"
-        set-option -g status-right "#[bg=colour237,fg=colour239 nobold, nounderscore, noitalics]#[bg=colour239,fg=colour246] %Y-%m-%d | %H:%M #[bg=colour239,fg=colour248,nobold,noitalics,nounderscore]#[bg=colour248,fg=colour237] #h "
-        set-window-option -g window-status-current-format "#[bg=colour214,fg=colour237,nobold,noitalics,nounderscore]#[bg=colour214,fg=colour239] #I #[bg=colour214,fg=colour239,bold] #W#{?window_zoomed_flag,*Z,} #[bg=colour237,fg=colour214,nobold,noitalics,nounderscore]"
-        set-window-option -g window-status-format "#[bg=colour239,fg=colour237,noitalics]#[bg=colour239,fg=colour223] #I #[bg=colour239,fg=colour223] #W #[bg=colour237,fg=colour239,noitalics]"
+        set -g status-position bottom
+        set -g status-justify centre
+        set -g status-style "bg=#1F1F28"
+        set -g window-style ""
+        set -g window-active-style ""
+        set -g status-left " #[fg=#8ec07c]#S"
+        set -g status-left-style ""
+        set -g status-left-length 50
+        set -g status-right "%a %d %b #[fg=#8ec07c]%R %Z "
+        set -g status-right-style "fg=#DCD7BA"
+        set -g status-right-length 25
+        set -g window-status-current-style "bold"
+        set -g window-status-style "fg=#DCD7BA"
+        set -g window-status-format " #[fg=#C8C093]#{?#{==:#W,zsh},[#I]#{b:pane_current_path},#W}#F "
+        set -g window-status-current-format " #[fg=#C8C093]#{?#{==:#W,zsh},#{b:pane_current_path},#W}#F "
+        set -g window-status-separator ""
+        set -g pane-active-border-style "fg=#DCD7BA"
+        set -g pane-border-style "fg=#DCD7BA"
         setw -g mode-keys vi
         bind -T copy-mode-vi v send -X begin-selection
         bind-key -T copy-mode-vi y send -X copy-pipe-and-cancel "xclip -sel clip -i"
