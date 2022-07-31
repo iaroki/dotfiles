cbbb01c164af936b3274f87d82a8de06dc60715e
Maxim Sytnyk <iaroki@protonmail.com>
2022-07-31 11:55:36 +0300

    add zathura config

--- nixos/home.nix
+++ nixos/home.nix
@@ -33,7 +33,6 @@ in
     home.packages = with pkgs; [
       tdesktop
       remmina
-      zathura
       scrot
       flameshot
       mpv
@@ -200,6 +199,50 @@ in
         bind-key -T copy-mode-vi y send -X copy-pipe-and-cancel "xclip -sel clip -i"
         bind P paste-buffer
       '';
+    };
+
+    programs.zathura = {
+        enable = true;
+        options = {
+          notification-error-bg   = "#f44747";
+          notification-error-fg   = "#DCD7BA";
+          notification-warning-bg = "#dcdcaa";
+          notification-warning-fg = "#11121D";
+          notification-bg         = "#11121D";
+          notification-fg         = "#af88F1";
+
+          completion-bg           = "#2d2d30";
+          completion-fg           = "#DCD7BA";
+          completion-group-bg     = "#2d2d30";
+          completion-group-fg     = "#DCD7BA";
+          completion-highlight-bg = "#062c45";
+          completion-highlight-fg = "#DCD7BA";
+
+          index-bg                = "#11121D";
+          index-fg                = "#DCD7BA";
+          index-active-bg         = "#af88e1";
+          index-active-fg         = "#282a36";
+
+          inputbar-bg             = "#ffaf00";
+          inputbar-fg             = "#11121D";
+          statusbar-bg            = "#11121D";
+          statusbar-fg            = "#ffffff";
+
+          highlight-color         = "#264f78";
+          highlight-active-color  = "#dcdcaa";
+
+          default-bg              = "#11121D";
+          default-fg              = "#DCD7BA";
+
+          render-loading          = true;
+          render-loading-fg       = "#11121D";
+          render-loading-bg       = "#DCD7BA";
+
+          recolor-lightcolor      = "#1F1F28";
+          recolor-darkcolor       = "#DCD7BA";
+
+          recolor                 = true;
+          };
     };
 
     home.file.dwm_autostart = {
