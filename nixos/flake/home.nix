3959d325f52238216cadcdcc1c1343d32addfb1e
Maxim Sytnyk <iaroki@protonmail.com>
2022-09-10 19:22:28 +0300

    attr

--- nixos/flake/home.nix
+++ nixos/flake/home.nix
@@ -19,6 +19,10 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
+  home.packages = [
+    attrs.note-sync.packages."x86_64-linux".note-sync
+  ];
+
   imports = [
     ./modules
   ];
