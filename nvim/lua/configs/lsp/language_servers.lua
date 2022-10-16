13621b162be711e605fdc865f9fd774524f8c546
iaroki <iaroki@protonmail.com>
2022-10-16 18:47:18 +0300

    change capabilities

--- nvim/lua/configs/lsp/language_servers.lua
+++ nvim/lua/configs/lsp/language_servers.lua
@@ -39,7 +39,7 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local servers = { 'gopls', 'ansiblels', 'bashls', 'dockerls', 'terraformls', 'tflint', 'yamlls', 'pyright' }
 for _, lsp in pairs(servers) do
   require('lspconfig')[lsp].setup {
