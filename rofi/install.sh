4b4ba4598eb1f7a5d924bce221382bb62059635e
Maxim Sytnyk <iaroki@protonmail.com>
2023-04-13 13:24:10 +0300

    rm rofi

--- rofi/install.sh
+++
@@ -1,9 +0,0 @@
-export CONFIG_DIR="~/.config/rofi"
-export THEMES_DIR="~/.local/share/rofi/themes"
-
-mkdir -p ${CONFIG_DIR}
-mkdir -p ${THEMES_DIR}
-
-cp -v config.rasi ${CONFIG_DIR}
-cp -v catppuccin.rasi ${THEMES_DIR}
-
