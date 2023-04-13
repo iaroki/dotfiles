24d8c3acb0b1da3690abb1cef52a36f6d6bf8c50
Maxim Sytnyk <iaroki@protonmail.com>
2023-04-13 15:11:41 +0300

    add treesitter parsers

--- nvim/lua/configs/treesitter/init.lua
+++ nvim/lua/configs/treesitter/init.lua
@@ -1,10 +1,11 @@
 require'nvim-treesitter.configs'.setup {
   -- ensure_installed = "maintained",
   ensure_installed = { "bash", "c", "cmake", "comment",
-  "cpp", "css", "dockerfile", "go", "gomod", "hcl", "html",
-  "http", "java", "javascript", "json", "kotlin", "lua",
-  "make", "markdown", "nix", "python", "regex", "ruby", "rust",
-  "toml", "tsx", "typescript", "vim", "yaml"},
+  "cpp", "css", "diff", "dockerfile", "go", "gomod", "gosum",
+  "git_config", "git_rebase", "gitattributes", "gitcommit", "gitignore",
+  "hcl", "html", "http", "ini", "javascript", "json", "lua", "make",
+  "markdown", "markdown_inline", "nix", "python", "regex", "ruby", "rust",
+  "sql", "sxhkdrc", "toml", "tsx", "terraform", "typescript", "vim", "yaml"},
   sync_install = false,
   -- ignore_install = { "javascript" },
 
