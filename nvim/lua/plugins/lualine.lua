13c901bd122c8bb18e311f9218b848fc6882f6b1
Maxim Sytnyk <iaroki@protonmail.com>
2023-04-18 11:03:46 +0300

    neovim: set lualine theme to auto

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,11 +1,11 @@
-return { 
+return {
   "nvim-lualine/lualine.nvim",
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
   opts = {
     options = {
-      theme = "tokyonight",
+      theme = "auto",
     },
   },
 }
