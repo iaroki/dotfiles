281f24e3f211f2bf2eacd77c3269bbac16306f2b
Maxim Sytnyk <iaroki@protonmail.com>
2023-06-09 11:50:40 +0300

    neovim: add diffview

--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -4,4 +4,37 @@ return {
     "nvim-lua/plenary.nvim",
     "sindrets/diffview.nvim"
   },
+  opts = {
+    disable_signs = false,
+    disable_hint = false,
+    disable_context_highlighting = false,
+    disable_commit_confirmation = true,
+    auto_refresh = true,
+    sort_branches = "-committerdate",
+    disable_builtin_notifications = false,
+    use_magit_keybindings = false,
+    kind = "tab",
+    console_timeout = 2000,
+    auto_show_console = true,
+    remember_settings = true,
+    use_per_project_settings = true,
+    commit_popup = {
+      kind = "split",
+    },
+    preview_buffer = {
+      kind = "split",
+    },
+    popup = {
+      kind = "split",
+    },
+    signs = {
+      -- { CLOSED, OPENED }
+      section = { ">", "v" },
+      item = { ">", "v" },
+      hunk = { "", "" },
+    },
+    integrations = {
+      diffview = true
+    },
+  },
 }
