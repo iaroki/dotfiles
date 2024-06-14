cd8b5392b1f321bebbb8361b50ea3cee3e7760cd
Maxim Sytnyk <iaroki@protonmail.com>
2024-06-14 11:12:17 +0300

    wezterm: add config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,48 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- Multiplexing
+local mux = wezterm.mux
+
+-- Changing the color scheme:
+local scheme = wezterm.get_builtin_color_schemes()['tokyonight_night']
+scheme.background = 'black'
+scheme.ansi = {"#15161e", "#f7768e", "#9ece6a", "#e0af68", "#7aa2f7", "#f5c2e7", "#cba6f7", "#a9b1d6"}
+scheme.brights = {"#414868", "#f7768e", "#9ece6a", "#e0af68", "#7aa2f7", "#f5c2e7", "#cba6f7", "#c0caf5"}
+config.color_schemes = {
+  ['tokyonight_black'] = scheme,
+}
+
+config.color_scheme = 'tokyonight_black'
+
+-- Appearance tweaks
+config.font = wezterm.font 'FiraCode Nerd Font'
+config.font_size = 12
+config.enable_tab_bar = false
+config.window_background_opacity = 0.95
+config.window_decorations = "NONE"
+config.window_padding = {
+  left   = 0,
+  right  = 0,
+  top    = 0,
+  bottom = 0,
+}
+
+wezterm.on('gui-attached', function(domain)
+  -- maximize all displayed windows on startup
+  local workspace = mux.get_active_workspace()
+  for _, window in ipairs(mux.all_windows()) do
+    if window:get_workspace() == workspace then
+      window:gui_window():maximize()
+    end
+  end
+end)
+
+-- Wayland crash workaround
+config.enable_wayland = false
+
+-- and finally, return the configuration to wezterm
+return config
