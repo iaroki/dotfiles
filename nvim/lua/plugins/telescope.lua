1fefafd04840bdb39b0b9355e70d16b1867995c0
Maxim Sytnyk <iaroki@protonmail.com>
2025-01-28 10:58:59 +0200

    feat(nvim): add gen.nvim and telescope-ui-select.nvim plugins

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,19 +1,22 @@
 return {
     "nvim-telescope/telescope.nvim",
-    -- cmd = "Telescope find_files",
-    -- change some options
-    opts = {
-      defaults = {
-        layout_strategy = "horizontal",
-        layout_config = { prompt_position = "top" },
-        sorting_strategy = "ascending",
-        winblend = 0,
-      },
-      extensions = {
-        "git_worktree",
-      },
-    },
     dependencies = {
       "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope-ui-select.nvim",
     },
+    config = function()
+      require("telescope").setup {
+        defaults = {
+          layout_strategy = "horizontal",
+          layout_config = { prompt_position = "top" },
+          sorting_strategy = "ascending",
+          winblend = 0,
+        },
+        extensions = {
+          "git_worktree",
+          "ui-select",
+        },
+      }
+      require("telescope").load_extension("ui-select")
+    end,
   }
