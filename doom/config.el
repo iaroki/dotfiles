f4f71d83d300299aaf05037a13cc07bd3e07df26
Maxim Sytnyk <iaroki@proton.me>
2025-05-26 10:09:19 +0300

    feat(emacs): improve workspace switch

--- doom/config.el
+++ doom/config.el
@@ -241,6 +241,9 @@
 (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
 (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
 (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
+(map! :leader
+      :desc "Switch to last workspace"
+      "TAB l" #'+workspace/other)
 
 ;; Indent lines
 (setq
